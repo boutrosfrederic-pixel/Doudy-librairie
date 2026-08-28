16fabac3f5a9568fb52f89894d228a07c3bdecc0
boutrosfrederic-pixel <boutros.frederic@gmail.com>
2026-08-28 23:47:25 +0200

    Delete app/src/main/java/com/doudy/librairie/AppDatabase.kt

--- app/src/main/java/com/doudy/librairie/AppDatabase.kt
+++
@@ -1,9 +0,0 @@
-
-package com.doudy.librairie
-import androidx.room.Database
-import androidx.room.RoomDatabase
-
-@Database(entities = [Book::class], version = 1)
-abstract class AppDatabase : RoomDatabase() {
-    abstract fun bookDao(): BookDao
-}
