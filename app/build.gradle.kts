03782917cabf834349ff832e7cf21bcf44922cda
boutrosfrederic-pixel <boutros.frederic@gmail.com>
2026-08-29 00:13:58 +0200

    Update build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -39,4 +39,5 @@ dependencies {
     implementation("androidx.camera:camera-camera2:1.3.4")
     implementation("androidx.camera:camera-lifecycle:1.3.4")
     implementation("androidx.camera:camera-view:1.3.4")
+    implementation("io.coil-kt:coil:2.7.0")
 }
